f5c86b097da81ec6db4f96de6af88bcb743e34f7
codergamer7 <90289698+codergamer7@users.noreply.github.com>
2026-06-13 22:46:33 -0400

    Initialize Express server with Gogoanime API routes

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const { ANIME } = require('@consumet/extensions');
+
+const app = express();
+app.use(cors());
+
+const gogo = new ANIME.Gogoanime();
+
+app.get('/search', async (req, res) => {
+  const data = await gogo.search(req.query.q);
+  res.json(data);
+});
+
+app.get('/info', async (req, res) => {
+  const data = await gogo.fetchAnimeInfo(req.query.id);
+  res.json(data);
+});
+
+app.get('/watch', async (req, res) => {
+  const data = await gogo.fetchEpisodeSources(req.query.id);
+  res.json(data);
+});
+
+app.listen(process.env.PORT || 3000);
